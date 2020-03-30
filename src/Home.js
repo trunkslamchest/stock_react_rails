17108800e180f28a0fc880475913b091c92d5e10
trunkslamchest <trunk9slamchest@gmail.com>
2020-03-30 12:05:29 -0400

    inital push to new branch

--- src/Home.js
+++ src/Home.js
@@ -17,6 +17,12 @@ export default class Home extends React.Component {
 		this.onClickUpdateTrafficFunctions(event)
 	}
 
+	onSubmitFunctions = (event) => {
+		event.preventDefault()
+		const formData = new FormData(event.target)
+		console.log(formData)
+	}
+
 	onClickUpdateTrafficFunctions = (event) => {
 		this.props.update_traffic_data({
 			user_id: this.props.user_id,
@@ -37,7 +43,6 @@ export default class Home extends React.Component {
 		const logged_in_home = <>
 			<h3>Index Template</h3>
 		</>
-
 
 		const log_in_button = [
 			<NavLink
@@ -72,9 +77,27 @@ export default class Home extends React.Component {
 			</div>
 		</>
 
+		const img_upload_form = <>
+			<form id="img_upload_form"
+				onSubmit={ this.onSubmitFunctions }
+			>
+				<input type="file"
+					name="img_input_upload"
+					id="img_input_upload"
+					className="img_input_upload"
+					accept="image/*"
+				/>
+				<input
+					type="submit"
+					value="Submit"
+				/>
+			</form>
+		</>
+
 		return(
 			<div className="default_wrapper">
 				{ localStorage.length === 0 ? logged_out_home : logged_in_home }
+				{ img_upload_form }
 			</div>
 		)
 	}
