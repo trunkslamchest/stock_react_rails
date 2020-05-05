4e7d3b8cb336740107fc9580574f3bedae0517e4
trunkslamchest <trunk9slamchest@gmail.com>
2020-05-05 12:58:24 -0400

    restructured footer fine print into multiple components

--- src/UI/footer/Footer.js
+++ src/UI/footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import FooterLinks from './footerLinks'
 import FooterLogos from './footerLogos'
-
+import FooterFinePrint from './footerFinePrint'
 
 import trafficFunctions from '../../utility/trafficFunctions'
 
@@ -26,19 +26,12 @@ const Footer = (props) => {
     trafficFunctions('element', 'http://localhost:3001/traffics', elementInfo)
   }
 
-  const fine_print =
-    <>
-      © 2019 Created by Austin Smith. All this_project™ logos and marks depicted herein are the property of this_project™ Enterprises and the respective employees and may not be reproduced without the prior written consent of this_project™ Enterprises, L.P. © this_project™ 2019. All Rights Reserved. The Zamboni word mark and configuration of the Zamboni ice resurfacing machine are registered trademarks of Frank J. Zamboni & Co., Inc.© Frank J. Zamboni & Co., Inc. 2019.All Rights Reserved. Any other third party trademarks or copyrights are the property of their respective owners. All rights reserved.
-    </>
-
   return(
     <div className="footer">
       <div className="footer_left">
         <FooterLinks onClickUpdateTrafficFunctions={ onClickUpdateTrafficFunctions } />
         <FooterLogos onClickUpdateTrafficFunctions={ onClickUpdateTrafficFunctions } />
-        <div className="fine_print">
-          { fine_print }
-        </div>
+        <FooterFinePrint />
       </div>
     </div>
   )
